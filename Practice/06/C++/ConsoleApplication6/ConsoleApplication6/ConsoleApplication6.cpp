e0bc0ca8983e0321312ef527c23abf7d7a7de3dc
Igr1k001 <Igorp1940@gmail.com>
2020-10-20 13:30:18 +0300

    Задание сделанное на паре(еще не закончил)

---
+++ Practice/06/C++/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double a, b, c, d, x1, x2; 
+    cout << "Введите коэффициенты a,b,c" << endl;
+    cin >> a >> b >> c; 
+    d = (b * b) - (4 * a * c); 
+    cout << "Дискриминант =" << d << endl; 
+    if (d == 0) 
+    {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        cout << "x =" << x1 << endl;
+    }
